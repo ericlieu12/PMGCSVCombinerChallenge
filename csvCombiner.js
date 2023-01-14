56e5810ed3e64becd4166f1bfac40a1cdbcbe50c
ericlieu12 <ericlieu118@gmail.com>
2023-01-14 12:00:37 -0500

    Correctly append new values to file

--- csvCombiner.js
+++ csvCombiner.js
@@ -1,26 +1,69 @@
 var fs = require('fs'),
 path = require('path')
-function writeToFile(name)
+
+function writeToCombinedFile(line, fileName, firstFile = false)
 {
-    fs.writeFile('Output.txt', data, (err) => {
+    console.log(firstFile)
+    data = formatLine(line, fileName)
+    if (firstFile)
+    {
+        data = line + "," + "\"filename\"" + "\n"
+        fs.appendFile('Output.csv', data, (err) => {
           
-        // In case of a error throw err.
-        if (err) throw err;
-    })
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+    }
+    else 
+    {
+        data = formatLine(line, fileName)
+        fs.appendFile('Output.csv', data, (err) => {
+          
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+    }
+    
+}
+function formatLine(line, fileName)
+{
+    content = line.split(',');
+    if (content.length != 2)
+    {
+        throw new Error;
+    }
+    
+    const newLine = appendToCSVLine(line, fileName)
+    
+    return newLine
+    
 }
 
+function appendToCSVLine(line, valueToAppend)
+{
+    line = line + "," + valueToAppend + "\n"
+    return line
+}
 function readFileIntoLines(name)
 {
     const readline = require('readline');
 
     void (async () => {
-    const rl = readline.createInterface({
-    input: fs.createReadStream('accessories.csv'),
-    crlfDelay: Infinity,
-    });
-
+        const rl = readline.createInterface({
+        input: fs.createReadStream('accessories.csv'),
+        crlfDelay: Infinity,
+        });
+    
+    var isFirst = true
     rl.on('line', (line) => {
-      console.log('line: ', line);
+        if (isFirst)
+        {
+            writeToCombinedFile(line, "accessories.csv", true);
+            isFirst = !isFirst
+        }
+        else {
+            writeToCombinedFile(line, "accessories.csv");
+        }
     });
 
     await new Promise((res) => rl.once('close', res));
@@ -31,9 +74,7 @@ function readFileIntoLines(name)
 
 const combineFiles = (a, b, c) => {
 
-    fileHandler.readFileIntoLines("accessories.csv")
+   readFileIntoLines("accessories.csv")
 }
-
-
 
 exports.combineFiles = combineFiles
