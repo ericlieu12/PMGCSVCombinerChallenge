e32e15053a68c7204c3886ef0e2446ff4fae1b12
ericlieu12 <ericlieu118@gmail.com>
2023-01-15 16:42:06 -0500

    Refactored some code to fix some bugs around error handling

--- csvCombiner.js
+++ csvCombiner.js
@@ -1,21 +1,27 @@
 var fs = require('fs'),
-    path = require('path')
+    path = require('path');
 var headerAlreadyWritten = false;
+var amountOfColumns = null;
+var headerValue = "";
+
 
 const combineFiles = async (arrayOfFileNames) => {
-
+    //reinitialize variables whenever combining files
+    headerValue = "";
+    headerAlreadyWritten = false;
+    amountOfColumns = null;
     //#region check for improper format of the arguments
     
     if (arrayOfFileNames.length == 0)
     {
-        console.log("Error, no files selected.")
+        console.log("Error: no files selected.")
         return;
     }
     for (const fileName of arrayOfFileNames)
     {
         if (fileName.substring(fileName.length - 4, fileName.length) != ".csv")
         {
-            console.log("Error, improper files.")
+            console.log("Error: improper files.")
             return;
         }
     }
@@ -24,11 +30,12 @@ const combineFiles = async (arrayOfFileNames) => {
 
     for (const fileName of arrayOfFileNames) {
        
+       
         await readFileandWriteToOutputFile(fileName).then(
             
         )
         .catch(error => {
-            console.log(`Error: ${error}`)
+            console.log(`${error} Stopping process now.`)
         })
 
     }
@@ -36,52 +43,72 @@ const combineFiles = async (arrayOfFileNames) => {
 }
 
 async function readFileandWriteToOutputFile(fileName) {
-
+    
     return new Promise((resolve, reject) => {
         const readline = require('readline');
-        const rl = readline.createInterface({
+        var rl = readline.createInterface({
             input: fs.createReadStream(fileName),
-            crlfDelay: Infinity,
+         
         });
-
         var isHeader = true
+
+      
         rl.on('line', (line) => {
-            try {
+               
                 if (!isLineCorrectlyFormatted(line)) {
-                    throw new Error("Not correctly formatted.");
+                    rl.emit("error", new Error("Not correctly formatted."))
+                    return;
                 }
+                
                 if (isHeader) {
                     isHeader = !isHeader
                     if (!headerAlreadyWritten) {
-                        data = formatHeader(line, fileName)
+                        data = formatHeader(line)
+                        setHeader(data)
                         writeToCombinedFile(data);
                         headerAlreadyWritten = !headerAlreadyWritten
+                    }
+                    else {
+                        if (headerValue != formatHeader(line))
+                        {
+                            
+                            rl.emit("error", new Error("Not the same header value."))
+                          
+                        }
+                        
                     }
                 }
                 else {
                     data = formatLine(line, fileName)
                     writeToCombinedFile(data);
                 }
-            }
-            catch (error)
-            {
-                reject(error);
-            }
+            
+          
             
         });
-
-        rl.on('close', function () {
+        
+        rl.on('close', function (error) {
+           
+                resolve();
+            
             //resolve promise if everything runs smoothly (i.e. running through entire file)
-            resolve();
+            
         });
         rl.on('error', function (error) {
+           
             //reject promise if error (error handled elsewhere)
+            rl.removeAllListeners();
+            //stop all listeners bc there is a buffer of lines
             reject(error);
+            
         });
 
     })
 
 
+}
+function setHeader(value) {
+    headerValue = value
 }
 function writeToCombinedFile(data) {
     //writes to the output file
@@ -89,18 +116,36 @@ function writeToCombinedFile(data) {
 }
 function formatHeader(line) {
     //formats header line of output file
+    amountOfColumns = line.split(",").length
     const newLine = `${line},\"filename\"`
     return newLine
 }
 function formatLine(line, fileName) {
+    if (fileName.includes("/"))
+    {
+        //then we know it is not the base path
+        fileName =  fileName.split("/")[fileName.split("/").length -1]
+        //get base path
+    }
     //formats normal line of output file
     const newLine = `${line},\"${fileName}\"`
     return newLine
 }
 function isLineCorrectlyFormatted(line) {
+
+    //#region precautionary measure
+    //column count set by format header, since header is the first to come in a file
+    //if not set already, call function to set it
+    if (amountOfColumns === null)
+    {
+        //get correct column amount
+        formatHeader(line)
+    }
+    //#endregion
+
     //checks if line is correctly formatted from input file
     lineSplit = line.split(',');
-    if (lineSplit.length != 2) {
+    if (lineSplit.length != amountOfColumns) {
         return false
     }
     return true
--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
 const csvCombiner = require('./csvCombiner');
 const fs = require('fs');
-test('formats header correctly', () => {
+test('formats header correctly', async () => {
   var testString = `"sample","sample"`
   expect(csvCombiner.formatHeader(testString)).toBe(`"sample","sample","filename"`);
 });
 
-test('formats line correctly', () => {
+test('formats line correctly', async () => {
   var testString = `"sample","sample"`
   var testFile = "sample.csv"
   expect(csvCombiner.formatLine(testString, testFile)).toBe(`"sample","sample","sample.csv"`);
 });
 
-test('checks line format correctly', () => {
+test('checks line format correctly', async () => {
   var testString = `"sample","sample"`
   var testString2 = `"sample","sample","sample"`
   expect(csvCombiner.isLineCorrectlyFormatted(testString)).toBe(true);
   expect(csvCombiner.isLineCorrectlyFormatted(testString2)).toBe(false);
 });
 
-test('checks writing to line works', () => {
+test('checks writing to line works', async () => {
   var testString = `"sample", "sample"`
   const logSpy = jest.spyOn(console, 'log');
 
@@ -27,27 +27,53 @@ test('checks writing to line works', () => {
   expect(logSpy).toHaveBeenCalledWith(testString);
 
 });
-
-test('checks combine file', async () => {
+test('checks combine file for 2 columns', async () => {
  
   const logSpy = jest.spyOn(console, 'log');
-  await csvCombiner.combineFiles(["smallsample.csv", "smallsample2.csv"])
+  await csvCombiner.combineFiles(["fixtures/smallsample.csv", "fixtures/smallsample2.csv"])
 
-  expect(logSpy).toHaveBeenCalledWith(`"big","big2","filename"`)
-  expect(logSpy).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Blouses","smallsample.csv"`)
-  expect(logSpy).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Shirts","smallsample.csv"`)
-  expect(logSpy).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Shoes","smallsample2.csv"`)
-  expect(logSpy).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Pants","smallsample2.csv"`)
+  await expect(logSpy).toHaveBeenCalledWith(`"big","big2","filename"`)
+  await expect(logSpy).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Blouses","smallsample.csv"`)
+  await expect(logSpy).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Shirts","smallsample.csv"`)
+  await expect(logSpy).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Shoes","smallsample2.csv"`)
+  await expect(logSpy).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Pants","smallsample2.csv"`)
 
  
 });
+
+test('checks combine file for 3 columns', async () => {
+ 
+  const logSpy3 = jest.spyOn(console, 'log');
+  await csvCombiner.combineFiles(["fixtures/smallsample3.csv", "fixtures/smallsample4.csv"])
+
+  await expect(logSpy3).toHaveBeenCalledWith(`"big","big2","big3","filename"`)
+  await expect(logSpy3).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Blouses","test","smallsample3.csv"`)
+  await expect(logSpy3).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Shirts","test","smallsample3.csv"`)
+  await expect(logSpy3).toHaveBeenCalledWith(`"b9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6","Blouses","t3est","smallsample4.csv"`)
+  await expect(logSpy3).toHaveBeenCalledWith(`"c2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7","Shirts","test2","smallsample4.csv"`)
+
+ 
+});
+
+test('checks combine file for different columns', async () => {
+ 
+  const logSpy3 = jest.spyOn(console, 'log');
+  await csvCombiner.combineFiles(["fixtures/smallsample3.csv", "fixtures/smallsample2.csv"])
+
+  await expect(logSpy3).toHaveBeenCalledWith(`Error: Not correctly formatted. Stopping process now.`)
+  
+
+ 
+});
+
+
 
 test('improper file not submitted', async () => {
  
   const logSpy = jest.spyOn(console, 'log');
   await csvCombiner.combineFiles(["smallsample.csa"])
 
-  expect(logSpy).toHaveBeenCalledWith(`Error, improper files.`)
+  await expect(logSpy).toHaveBeenCalledWith(`Error: improper files.`)
 
  
 });
@@ -57,7 +83,7 @@ test('no file will throw error', async () => {
   const logSpy = jest.spyOn(console, 'log');
   await csvCombiner.combineFiles([])
 
-  expect(logSpy).toHaveBeenCalledWith(`Error, no files selected.`)
+  await expect(logSpy).toHaveBeenCalledWith(`Error: no files selected.`)
 
  
 });
